f2120fe4657e095d7f5f9ff7ed063ba7140c42b6
Morgane <morganephilo@gmail.com>
2024-10-11 17:05:39 +0200

    init server

--- server.js
+++ server.js
@@ -1,3 +1,9 @@
 const bcrypt = require('bcrypt'); // Pour gérer les hashs des mots de passe
 const users = require('./users.json'); // Contient les utilisateurs et hash des mots de passe
 const data = require('./data.json'); // Données à renvoyer si l'authentification réussit
+const http = require('node:http');
+
+const server = http.createServer((req, res) => {
+});
+
+server.listen(3000);
